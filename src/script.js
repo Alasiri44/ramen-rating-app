621e38bea1305e518dac2286aab4baa2b4e0fcf6
alasiri <austinalasiri44@gmail.com>
2025-03-17 20:20:25 +0300

    Added functions

--- src/script.js
+++ src/script.js
@@ -6,47 +6,58 @@ document.addEventListener('DOMContentLoaded', function(e){
         { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
      ];
 
-     
-     // Creating a new image element
      const button = document.querySelector('#myButton');
-     const images = document.getElementById('images');
-     button.addEventListener('click', function(e){
-        e.preventDefault();
+     const images = document.getElementById('ramen-menu');
 
-        const ramenName = document.getElementById('ramenName').value;
-        const ramenRestaurant = document.getElementById('ramenRestaurant').value;
-        const ramenImage = document.getElementById('ramenImage').value;
-        const ramenRating= document.getElementById('ramenRating').value;
-        const ramenComment = document.getElementById('ramenComment').value; 
-
-        if(ramenName.trim() !== '' && ramenRestaurant.trim() !== ''  && ramenImage.trim() !== ''  && ramenRating.trim() !== ''  && ramenComment.trim() !== '' ){
-            const newImage = document.createElement('img');
-            newImage.src = ramenImage;
-            newImage.alt = ramenName;
-            newImage.classList.add("ramen-image");
-
-            images.appendChild(newImage);
-        }else{
-         alert('Please fill all the requirements');
-        }        
-     });
-
-
-     // Selecting the images
-     const myRamen = document.querySelectorAll('div#images img');
+ //Function to display the ramens
+   function displayRamens(){
      
-     
-     images.addEventListener('click', function(event){
-        
-      if (event.target.classList.contains("ramen-image")) {
-        const myDiv = document.getElementById('enlargedImage');
-        
-        myDiv.src = event.target.src;
+      images.addEventListener('click', handleClick);
+         
+         
+      function handleClick(event){        
+         if (event.target.classList.contains("ramen-image")) {
+         const myDiv = document.getElementById('enlargedImage');
+         
+         myDiv.src = event.target.src;
 
-        myDiv.style.height = '250px';
-        myDiv.style.width = '300px';
+         myDiv.style.height = '250px';
+         myDiv.style.width = '300px';
+         }
       }
-     });
-     
+   }
 
+     
+// Function addSubmitListener to create new elements
+     function addSubmitListener(){
+
+
+      button.addEventListener('click', function(e){
+         e.preventDefault();
+
+         const ramenName = document.getElementById('ramenName').value;
+         const ramenRestaurant = document.getElementById('ramenRestaurant').value;
+         const ramenImage = document.getElementById('ramenImage').value;
+         const ramenRating= document.getElementById('ramenRating').value;
+         const ramenComment = document.getElementById('ramenComment').value; 
+
+         if(ramenName.trim() !== '' && ramenRestaurant.trim() !== ''  && ramenImage.trim() !== ''  && ramenRating.trim() !== ''  && ramenComment.trim() !== '' ){
+               const newImage = document.createElement('img');
+               newImage.src = ramenImage;
+               newImage.alt = ramenName;
+               newImage.classList.add("ramen-image");
+
+               images.appendChild(newImage);
+         }else{
+            alert('Please fill all the requirements');
+         }        
+      });
+   }  
+
+   function main(){
+      displayRamens();
+      addSubmitListener();
+   }
+
+   main();
 });
