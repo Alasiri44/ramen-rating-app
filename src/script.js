21e2d55dc0abb493d1ca30b81caabdbe9abb1b36
alasiri <austinalasiri44@gmail.com>
2025-03-17 18:51:54 +0300

    Added a bit of styling

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(e){
     const ramens = [
         { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
         { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
@@ -18,5 +18,26 @@ document.addEventListener('DOMContentLoaded', function(){
         myDiv.style.height = '250px';
         myDiv.style.width = '300px';
      });
+     });
+
+     // Creating a new image element
+     const button = document.querySelector('#myButton');
+     button.addEventListener('click', function(e){
+        e.preventDefault();
+
+        const ramenName = document.getElementById('ramenName').value;
+        const ramenRestaurant = document.getElementById('ramenRestaurant').value;
+        const ramenImage = document.getElementById('ramenImage').value;
+        const ramenRating= document.getElementById('ramenRating').value;
+        const ramenComment = document.getElementById('ramenComment').value; 
+
+        if(ramenName.trim() !== '' && ramenRestaurant.trim() !== ''  && ramenImage.trim() !== ''  && ramenRating.trim() !== ''  && ramenComment.trim() !== '' ){
+        
+        }else{
+         alert('Please fill all the requirements');
+        }
+
+
+       
      });
 });
