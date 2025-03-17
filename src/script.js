cb19af5c16d29258ed961f1dbfe0175002e34c50
alasiri <austinalasiri44@gmail.com>
2025-03-17 12:27:38 +0300

    Finished the basic styling

--- src/script.js
+++ src/script.js
@@ -1,5 +1,22 @@
-const ramens = [
-    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
- ];
+
+document.addEventListener('DOMContentLoaded', function(){
+    const ramens = [
+        { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
+        { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
+        { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+     ];
+
+     const myRamen = document.querySelectorAll('div#images img');
+     
+     myRamen.forEach(ramenImage => {
+     ramenImage.addEventListener('click', function(){
+        
+        const myDiv = document.getElementById('enlargedImage');
+        
+        myDiv.src = ramenImage.src;
+
+        myDiv.style.height = '250px';
+        myDiv.style.width = '300px';
+     });
+     });
+});
