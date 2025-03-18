28a438881bd81ec5d75c3a12003b31beb96ec0e0
alasiri <austinalasiri44@gmail.com>
2025-03-18 10:36:39 +0300

    Allowed removing elements

--- src/script.js
+++ src/script.js
@@ -1,9 +1,12 @@
 
 document.addEventListener('DOMContentLoaded', function(e){
     const ramens = [
-        { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
-        { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-        { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+        { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "https://moringa.instructure.com/courses/967/files/517801/preview", rating: 5, comment: "Delicious!" },
+        { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "https://moringa.instructure.com/courses/967/files/517800/preview", rating: 4, comment: "Very flavorful!" },
+        { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "https://moringa.instructure.com/courses/967/files/517799/preview", rating: 4, comment: 'The best' },
+        {id: 4, name: 'Narotu Ramen', restaurant: 'Mahashi', image: 'https://moringa.instructure.com/courses/967/files/517798/preview', rating: 5, comment: 'Fabulous' },
+        {id: 5, name: 'Mahak Ramen', restaurant: 'Mahaja', image: 'https://moringa.instructure.com/courses/967/files/517797/preview', rating: 1, comment: 'Bad choice' }
+       
      ];
 
      const button = document.querySelector('#myButton');
@@ -11,6 +14,32 @@ document.addEventListener('DOMContentLoaded', function(e){
 
  //Function to display the ramens
    function displayRamens(){
+      const imgDiv = document.getElementById('ramen-menu');
+      for(let element of ramens){
+         let img = document.createElement('img');
+         img.src = element.image;
+         // img.alt = element.name;
+         img.classList.add('ramen-image');
+
+         let removeButton = document.createElement('button');
+         removeButton.classList.add('remove-button');
+         removeButton.innerText = 'X';         
+
+         imgDiv.appendChild(removeButton);
+         imgDiv.appendChild(img);       
+
+         removeButton.style.position = 'absolute';
+      }
+
+      let removeButton = document.querySelectorAll('.remove-button');
+      removeButton.forEach(buttonElement=> {
+         buttonElement.addEventListener('click', function(event){
+            event.target.nextSibling.remove();
+            
+         });
+      });
+      
+
      
       images.addEventListener('click', handleClick);
          
@@ -47,17 +76,21 @@ document.addEventListener('DOMContentLoaded', function(e){
                newImage.alt = ramenName;
                newImage.classList.add("ramen-image");
 
+               ramens.push({id: (ramens[ramens.length - 1].id)++, name: ramenName, restaurant: ramenRestaurant, image: ramenImage, rating: ramenRating, comment: ramenComment});
+              
                images.appendChild(newImage);
          }else{
             alert('Please fill all the requirements');
-         }        
+         }                          
       });
    }  
 
-   function main(){
+
+   //main function to invoke the displayRamens() function and addSubmitListener() function
+   (function main(){
       displayRamens();
       addSubmitListener();
-   }
+   })();
 
-   main();
+ 
 });
